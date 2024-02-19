6f36120779335438187a5a7b0025aab133100565
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-02-20 00:19:43 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
