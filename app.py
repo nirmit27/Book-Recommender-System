14ba0cedaac3fb5beb972261821ee71fd6ba0e39
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-02-20 16:07:14 +0000

    top 50 books home page no corrections yet

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, url_for
+import pandas as pd
+
+df = pd.read_csv('datasets/top50.csv')
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Book Recommender System")
+    return render_template('index.html', title="Top 50 Books",
+                           book = list(df['Book-Title'].values[:50]),
+                           author = list(df['Book-Author'].values[:50]),
+                           img = list(df['Image-URL-M'].values[:50]),
+                           votes = list(df['num_rating'].values[:50]),
+                           rating = list(df['avg_rating'].values[:50]))
 
 
 if __name__ == "__main__":
